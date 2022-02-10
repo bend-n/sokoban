22442a5359dc8cdb0c1f664ba1d826c787cc80e0
bendn <bend.n@outlook.com>
2022-02-10 13:54:44 +0700

    magic lite!!!!

--- LoadingScreen.gd
+++ LoadingScreen.gd
@@ -4,15 +4,33 @@ onready var animate : AnimationPlayer = $animate
 onready var progress = $Container/occluder/Progressbar
 onready var tween = $Container/Tween
 onready var fade = $fade
-onready var center = $Container.get_viewport_rect().size / 2
-
 var queued = false
 
+const distance = 150
+
 signal startup_complete
-#signal exit_complete
 
 func _ready():
-	$Container/occluder.position = center
+	setup_curve(center($Container/occluder), $Container/Path)
+	# sets up the curve with the return value from the centering of the occluder
+
+func setup_curve(center, path):
+	var curve := Curve2D.new()
+	curve.clear_points()
+	
+	var top_left = center + Vector2(-distance, -distance)
+	var bottom_left = center + Vector2(-distance, distance)
+	var top_right = center + Vector2(distance, -distance)
+	var bottom_right = center + Vector2(distance, distance)
+	
+	for corner in [top_left, top_right, bottom_right, bottom_left, top_left]:
+		curve.add_point(corner)
+	
+	path.set_curve(curve)
+
+func center(node):
+	node.position = $Container.get_viewport_rect().size / 2
+	return node.position 
 
 func startup():
 	_set_disable_inputs(true)
