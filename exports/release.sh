df6731111b59025897a2bb83dbe37e2a8c25e82b
bendn <bend.n@outlook.com>
2022-02-13 09:08:37 +0700

    working on new release systematica

--- exports/release.sh
+++ exports/release.sh
@@ -2,8 +2,21 @@
 
 release_path=~/sokoban/exports
 
-if [ "$1" == "-y" ]; then
-    yes | release -e sokoban "$release_path/linux" "$release_path/windows" "$release_path/mac" "$release_path/html" $1
-else
-    release -e sokoban "$release_path/linux" "$release_path/windows" "$release_path/mac" "$release_path/html" $1
-fi
+function download_and_push() {
+    echo "Downloading $1"
+    wget $1 -q -P $2
+    butler push $2 $3:$4
+    rm -rf "${2:?}/"*
+}
+
+owner=bendnuts
+repository=sokoban
+
+path="https://github.com/$owner/$repository/releases/latest/download"
+
+itch_path=bendn/sokoban
+
+download_and_push "$path/Linux.zip" "$release_path/linux" "$itch_path" linux
+download_and_push "$path/HTML.zip" "$release_path/html" "$itch_path" html
+download_and_push "$path/Windows.zip" "$release_path/windows" "$itch_path" windows
+download_and_push "$path/Mac.zip" "$release_path/mac" "$itch_path" mac
